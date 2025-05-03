a9c6a8fa35a43f7038277053d2c60276c754b6dc
OriginalNils <nils-doering@gmx.de>
2025-05-03 13:16:20 +0200

    Finished Aufg02

--- Blatt03/blatt03.jl
+++ Blatt03/blatt03.jl
@@ -67,3 +67,51 @@ function aufg01(N,M,m)
     end
     plot_stueckweise_interpolation(N, M, m)
 end
+
+function aufg02(N, M; output_folder="output")
+    # Runge-Funktion
+    runge(x) = 1 / (1 + 25x^2)
+
+    # Gitter wie in aufg01
+    function diskretisiere(N, M)
+        N % M == 0 || error("N muss ein Vielfaches von M sein.")
+        return collect(range(-1, 1, length=N+1))
+    end
+
+    # Ursprüngliches Gitter und Funktionswerte
+    x = diskretisiere(N, M)
+    f = runge.(x)
+
+    # Lineare Spline-Plot
+    plt = plot(x, f, label="Lineare Spline-Approximation", lw=2, color=:red, marker=:circle)
+    plot!(runge, -1, 1, label="Runge-Funktion", lw=2, color=:blue, linestyle=:dash)
+
+    # Intervallmittelpunkte berechnen
+    x_mid = [(x[i] + x[i+1])/2 for i in eachindex(x[1:end-1])]
+
+    # Spline-Auswertung an den Mittelwerten
+    f_spline_mid = Float64[]
+    for i in eachindex(x[1:end-1])
+        xi, xi1 = x[i], x[i+1]
+        fi, fi1 = f[i], f[i+1]
+        t = (x_mid[i] - xi) / (xi1 - xi)
+        push!(f_spline_mid, (1 - t)*fi + t*fi1)
+    end
+
+    # Exakte Werte und Fehler
+    f_exact_mid = runge.(x_mid)
+    fehler = abs.(f_spline_mid .- f_exact_mid)
+
+    # Fehlerplot
+    plt_error = plot(x_mid, fehler, label="|Spline - Runge|", lw=2, color=:green,
+        title="Absoluter Fehler an Intervallmittelpunkten", xlabel="x", ylabel="Fehler")
+
+    # Speichern
+    isdir(output_folder) || mkpath(output_folder)
+    savefig(plt, joinpath(output_folder, "plot3_linearspline_vs_runge.png"))
+    savefig(plt_error, joinpath(output_folder, "plot4_spline_error_midpoints.png"))
+
+    println("Spline-Plot und Fehlerplot gespeichert in $output_folder.")
+end
+
+
